b525e35d40a9a3a8002b89ee96a1f6f58acb735d
Anthony Palmer <anthony@motivislearning.com>
2017-10-06 20:07:53 -0400

    awdwd

--- modules/soundVolume.js
+++ modules/soundVolume.js
@@ -5,9 +5,9 @@ module.exports = (bot) => {
   bot.on('message', async (user, userID, channelID, message, event) => {
     if (message.startsWith('$louder')) {
       let sound = message.replace('$louder', '').trim()
-      cmd.get(`sox -v 1.4 /home/pi/node/brave-bot/soundFiles/${sound}.ogg /home/pi/node/brave-bot/soundFiles/${sound}2.ogg`, () => {
-        cmd.get(`rm -f /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
-          cmd.get(`mv /home/pi/node/brave-bot/soundFiles/${sound}2.ogg /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
+      cmd.get(`sudo sox -v 1.4 /home/pi/node/brave-bot/soundFiles/${sound}.ogg /home/pi/node/brave-bot/soundFiles/${sound}2.ogg`, () => {
+        cmd.get(`sudo rm -f /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
+          cmd.get(`sudo mv /home/pi/node/brave-bot/soundFiles/${sound}2.ogg /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
             bot.sendMessage({
               to: userID,
               message: `${sound} done`
@@ -19,9 +19,9 @@ module.exports = (bot) => {
 
     if (message.startsWith('$quieter')) {
       let sound = message.replace('$quieter', '').trim()
-      cmd.get(`sox -v 0.6 /home/pi/node/brave-bot/soundFiles/${sound}.ogg /home/pi/node/brave-bot/soundFiles/${sound}2.ogg`, () => {
-        cmd.get(`rm -f /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
-          cmd.get(`mv /home/pi/node/brave-bot/soundFiles/${sound}2.ogg /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
+      cmd.get(`sudo sox -v 0.6 /home/pi/node/brave-bot/soundFiles/${sound}.ogg /home/pi/node/brave-bot/soundFiles/${sound}2.ogg`, () => {
+        cmd.get(`sudo rm -f /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
+          cmd.get(`sudo mv /home/pi/node/brave-bot/soundFiles/${sound}2.ogg /home/pi/node/brave-bot/soundFiles/${sound}.ogg`, () => {
             bot.sendMessage({
               to: userID,
               message: `${sound} done`
