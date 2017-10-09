144600094cf7e030c38ba54cc72a992192cbb5e8
Anthony Palmer <anthony@motivislearning.com>
2017-10-09 14:16:53 -0400

    awdw

---
+++ modules/terminalchat.js
@@ -0,0 +1,30 @@
+const server = require('http').createServer() // eslint-disable-line no-unused-vars
+const io = require('socket.io')(420, {
+  path: '/test',
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+})
+
+module.exports = (bot) => {
+  io.on('connection', socket => {
+    console.log('tanners connected')
+
+    // incoming messages from me
+    socket.on('chat', data => {
+      bot.sendMessage({
+        to: '272692254249058304',
+        message: data
+      })
+    })
+
+    // outgoing messages to me
+    bot.on('message', async (user, userID, channelID, message, event) => {
+      if (bot.id === userID) return
+
+      socket.emit('chat', message)
+    })
+  })
+}
